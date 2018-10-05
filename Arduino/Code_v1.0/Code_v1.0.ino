76de7bb244726ef21316bbf5df629d1a54393d47
franxi2953 <franxi2953@gmail.com>
2018-10-05 20:14:55 +0200

    zsdfsf

--- Arduino/Code_v1.0/Code_v1.0.ino
+++ Arduino/Code_v1.0/Code_v1.0.ino
@@ -1,22 +1,41 @@
 #include "iGEM-Madrid-OLM.h"
 
 msgReceiver serial;
-motorHandler motorHandler_(4076,8,9,10,11);
+msgReceiver serial2;
+
+motorHandler motorHandler_(4076, 8, 9, 10, 11);
 
 void setup() {
+
+  //PC-Arduino
   Serial.begin(9600);
+  //Arduino-rodeostat
+  Serial2.begin(115200);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Handle the motors
+  motorHandler_.runMotors();
 }
 
 void serialEvent() {
-  
- delay(50);
-  
- if (serial.receiver() == ERR_PARS)
+  delay(50);
+
+  if (serial.receiver() != 0)
     Serial.println("ERROR:  There was a problem parsing the string");
- 
- serial.understand(motorHandler_);
+
+  serial.understand(motorHandler_);
 }
+
+void SerialEvent2() {
+  delay(5);
+
+  if (serial.receiver() != 0)
+    Serial.println("ERROR:  There was a problem receiving rodeostat message");
+
+  String msg = "";
+  serial2.root.printTo(msg);
+  Serial.println(msg);
+}
+
